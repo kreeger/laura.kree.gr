35ae908a2be2d01942121ec55769555c2e453ef8
Ben Kreeger <ben@kree.gr>
2013-02-17 17:13:42 -0600

    Initial.

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'mongoid'
+gem 'slim'
+gem 'activeadmin-mongoid'
+gem 'devise'
+gem 'sass-rails',   '~> 3.2.3'
+gem 'coffee-rails', '~> 3.2.1'
+gem 'compass-rails'
+gem 'uglifier', '>= 1.0.3'
+gem 'turbo-sprockets-rails3'
+
+group :development do
+  gem 'puma'
+  gem 'debugger'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Laura::Application.config.secret_token = 'ec5eda6898087522aeec8d0daf332cccefb7cd1bee8869200f8393f360e393f1a7d1d1a32dfb2d6dc4357c5d4b0ec8457b66d3384aa8afcd80a3b19ceb672ed6'
